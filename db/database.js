8acf538c4958842af8c64aed6f41930c0a95a87f
adnan <sirgeant10@gmail.com>
2025-03-18 22:25:30 +0600

    Add env variaables to connect.

--- db/database.js
+++ db/database.js
@@ -2,10 +2,10 @@ import mysql from "mysql2";
 
 const pool = mysql
   .createPool({
-    host: "localhost",
-    user: "root",
-    password: "Area90448",
-    database: "schoolapi",
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
   })
   .promise();
 
