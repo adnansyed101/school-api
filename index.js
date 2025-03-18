aa1d0ac7fe26b0e2fa6a0e6bbf3105f2fdcbc487
adnan <sirgeant10@gmail.com>
2025-03-18 23:41:30 +0600

    Move dotenv to db js.

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 import express from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
+import { getSchools, createSchool } from "./db/queries.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+console.log(process.env.PORT);
 
 app.use(express.json());
 
 app.get("/", (_req, res) => {
   res.send("Hello From SchoolApi");
+});
+
+app.get("/listSchools", async (_req, res) => {
+  const schools = await getSchools();
+  console.log(schools);
+
+  res.send(schools);
 });
 
 app.use((err, _req, res, _next) => {
